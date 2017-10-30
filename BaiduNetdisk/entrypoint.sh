5a093b8b508360c3a197311b4460e065250a47bf
John Shine <mr.john.shine@gmail.com>
2017-10-30 21:22:59 +0800

    fix vnc unable kill

--- BaiduNetdisk/entrypoint.sh
+++ BaiduNetdisk/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-[ -z "${DISPLAY}" ] || /usr/bin/vncserver -kill ${DISPLAY} && sleep 3
+[ -z "${DISPLAY}" ] || /usr/bin/vncserver -kill ${DISPLAY}; rm -f /tmp/.X*-lock; rm -rf /tmp/.X11-unix/; sleep 2
 /usr/bin/vncserver -geometry 1920x1080 -fg
