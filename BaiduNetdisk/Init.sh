a22eefd8a8dba82aa0bbb014b31cb4f8a4eee3cc
johnshine <mr.john.shine@gmail.com>
2017-11-14 23:46:37 +0800

    finish ie8 setup

--- BaiduNetdisk/Init.sh
+++ BaiduNetdisk/Init.sh
@@ -24,28 +24,23 @@ ${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" Reg.exe add "HKCU\Software\Wine\DllO
 ${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "urlmon" /t REG_SZ /d "native, builtin" /f
 ${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "inetcpl.cpl" /t REG_SZ /d "native, builtin" /f
 
-${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'msxml3=n' -- 'msiexec.exe' '/i' '/tmp/msxml3.msi' '/quiet'
+env LC_ALL="en_US.UTF-8" ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'msxml3=n' -- 'msiexec.exe' '/i' '/tmp/msxml3.msi' '/quiet'
 
 # Remove the fake DLLs from the existing WINEPREFIX
 if [ -f "${HOME}/.cxoffice/${BOTTLE}/drive_c/Program Files/Internet Explorer/iexplore.exe" ]; then
     if grep -q -i "wine placeholder" "${HOME}/.cxoffice/${BOTTLE}/drive_c/Program Files/Internet Explorer/iexplore.exe"; then
         echo 'unregserver iexplore'
-        # ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console iexplore -unregserver
-        ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} iexplore -unregserver
+        ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console iexplore -unregserver
     fi
     echo 'backup iexplore.exe'
     mv "${HOME}/.cxoffice/${BOTTLE}/drive_c/Program Files/Internet Explorer/iexplore.exe" "${HOME}/.cxoffice/${BOTTLE}/drive_c/Program Files/Internet Explorer/iexplore.exe.bak"
 fi
 
-for dll in browseui inseng itircl itss jscript msctf mshtml shdoclc shdocvw shlwapi urlmon; do
-    test -f "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32"/${dll}.dll && mv "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32"/$dll.dll "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32"/${dll}.dll.bak
-done
-
 # See https://bugs.winehq.org/show_bug.cgi?id=16013
 mkdir -p "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32/catroot/{f750e6c3-38ee-11d1-85e5-00c04fc295ee}"
 sudo mv /tmp/installers/exe/winetest.cat "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32/catroot/{f750e6c3-38ee-11d1-85e5-00c04fc295ee}/oem0.cat" && sudo chown ${UID}:${GID} "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32/catroot/{f750e6c3-38ee-11d1-85e5-00c04fc295ee}/oem0.cat"
 
-${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'iexplore.exe=n' '--' '/tmp/installers/exe/IE8-WindowsXP-x86-ENU.exe' '/passive' '/update-no' '/forcerestart'
+env LC_ALL="en_US.UTF-8" ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'iexplore.exe=n' '--' '/tmp/installers/exe/IE8-WindowsXP-x86-ENU.exe' '/passive' '/update-no' '/forcerestart'
 
 # Work around DLL registration bug until ierunonce/RunOnce/wineboot is fixed
 # FIXME: whittle down this list
@@ -55,13 +50,13 @@ done
 
 ${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" Reg.exe add "HKCU\Software\Microsoft\Internet Explorer\Main" /v "TabProcGrowth" /t REG_DWORD /d "00000000" /f
 
-${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" --untrusted --wait-children --no-convert --new-console -- '/tmp/installers/exe/IE8-WindowsXP-KB2936068-x86-ENU.exe' '/quiet /forcerestart'
-status=$?
-case $status in
-  0|194) ;;
+env LC_ALL="en_US.UTF-8" ${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" --untrusted --wait-children --no-convert --new-console -- '/tmp/installers/exe/IE8-WindowsXP-KB2936068-x86-ENU.exe' '/quiet' '/forcerestart'
+case $? in
+  0|194) echo "IE8-WindowsXP-KB2936068-x86-ENU installation successfully"
+    ;;
   *) echo "IE8-WindowsXP-KB2936068-x86-ENU installation failed"
+    ;;
 esac
-
 
 ${INSTALLDIR}/bin/cxassoc --bottle ${BOTTLE} --sync
 # 开始菜单快捷方式
@@ -70,7 +65,7 @@ ${INSTALLDIR}/bin/cxmenu --bottle ${BOTTLE} --sync --mode 'install'
 ${INSTALLDIR}/bin/cxassoc --bottle ${BOTTLE} --sync --sync-mode --mode 'mime;default=\.asx:\.wma:\.wmx:\.asf:\.wm:\.wmv:\.wax;alternative=\.avi:\.mov:\.qt'
 ${INSTALLDIR}/bin/cxnsplugin --bottle ${BOTTLE} --sync --mode 'ignore'
 # ${INSTALLDIR}/bin/wine --scope 'private' --no-convert --wl-app 'assocscan.exe' --scan --icon-dir '/home/crossover/.cxoffice/BaiduNetDisk/windata/Associations'
-${INSTALLDIR}/bin/wine --scope 'private' --wl-app 'pluginserver.exe' '--scandlls'
+# ${INSTALLDIR}/bin/wine --scope 'private' --wl-app 'pluginserver.exe' '--scandlls'
 
 #  wget https://raw.githubusercontent.com/john-shine/Docker-CodeWeavers_CrossOver-VNC/master/BaiduNetdisk/BaiduNetdisk.tar.xz -O /tmp/BaiduNetdisk.tar.xz && \
 tar -Jxf /tmp/installers/BaiduNetdisk.tar.xz -C ${HOME}/.cxoffice/${BOTTLE}/drive_c/Program\ Files/
@@ -78,5 +73,4 @@ sudo mv /tmp/installers/dll/riched20.dll ${HOME}/.cxoffice/${BOTTLE}/drive_c/win
 sudo mv /tmp/installers/dll/riched32.dll ${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32/
 sudo mkdir -p /mnt/drive_d/BaiduNetdiskDownload && sudo chown ${UID}:${GID} -R /mnt/drive_d
 ln -sf /mnt/drive_d "${HOME}/.cxoffice/${BOTTLE}/dosdevices/d:"
-${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --wait-children --wl-app 'reboot.exe'
-# env LC_ALL="en_US.UTF-8" ${INSTALLDIR}/bin/cxreboot --bottle ${BOTTLE}
+env LC_ALL="en_US.UTF-8" ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --wait-children --wl-app 'reboot.exe'
