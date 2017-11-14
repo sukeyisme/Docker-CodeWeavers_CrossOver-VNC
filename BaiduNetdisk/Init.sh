0f718bb3df07154e5f7676243cff18ce4ec1322b
johnshine <mr.john.shine@gmail.com>
2017-11-14 18:10:45 +0800

    before test

--- BaiduNetdisk/Init.sh
+++ BaiduNetdisk/Init.sh
@@ -4,29 +4,82 @@ ${INSTALLDIR}/bin/cxbottle --create --bottle "${BOTTLE}" --description "百度�
 
 sed -i -e 's/;;"AllowUntrusted" = ""/"AllowUntrusted" = "1"/' ${HOME}/.cxoffice/${BOTTLE}/cxbottle.conf
 
-Starting: '/opt/cxoffice/bin/cxassoc' --bottle ${BOTTLE} '--sync'
-Starting: '/opt/cxoffice/bin/wine' '--scope' 'private' '--no-convert' '--wl-app' 'assocscan.exe' '--scan' '--icon-dir' '/home/crossover/.cxoffice/BaiduNetDisk/windata/Associations'
-Starting '/opt/cxoffice/bin/wineloader' 'winewrapper.exe' '--no-convert' '--run' '--'
-Starting '/opt/cxoffice/bin/wineloader' 'winewrapper.exe' '--wait-children' '--no-convert' '--run' '--'
-
 for font in /tmp/fonts/*; do
   ${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" --untrusted --wait-children --no-convert -- 'cxinstallfonts.exe' "${font}"
 done
 
 env LC_ALL="en_US.UTF-8" ${INSTALLDIR}/bin/wine --bottle "${BOTTLE}" --untrusted --wait-children --no-convert --new-console -- "/tmp/installers/exe/Firefox Setup 20.0_zh-CN.exe" "-ms" && rm -f "/tmp/Firefox Setup 20.0.exe"
 
-  ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'msxml3=n' -- 'msiexec.exe' '/i' '/tmp/msxml3.msi'
-  ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'iexplore.exe=n' '--' '/tmp/IE8-WindowsXP-x86-ENU.exe'
-  # 开始菜单快捷方式
-  ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --wait-children --wl-app cxmklnk.exe -- --lnkfile 'C:\users\crossover\Start Menu//Internet Explorer.lnk' --target 'C:\Program Files/Internet Explorer/iexplore.exe'
-  # ${INSTALLDIR}/bin/wineshelllink --utf8 --root 'C:\users\crossover\Start Menu' --link 'C:\users\crossover\Start Menu\Internet Explorer.lnk' '--path' 'C:\Program Files\Internet Explorer\iexplore.exe' '--menu' '--icon' 'FB4C_iexplore.0'
-  # ${INSTALLDIR}/bin/cxmenu '--utf8' '--create' 'StartMenu.C^3A_users_crossover_Start+Menu/Internet Explorer.lnk' '--type' 'windows' '--install' '--icon' 'FB4C_iexplore.0' '--shortcut' 'iexplore'
-  # ${INSTALLDIR}/bin/cxmenu --bottle ${BOTTLE} '--sync' '--mode' 'install'
-  ${INSTALLDIR}/bin/wine '--scope' 'private' '--no-convert' '--wl-app' 'winemenubuilder.exe' '--cx-dump-menus' '--cx-all-menus'
-  ${INSTALLDIR}/bin/cxassoc --bottle ${BOTTLE} --sync --sync-mode --mode 'mime;default=\.asx:\.wma:\.wmx:\.asf:\.wm:\.wmv:\.wax;alternative=\.avi:\.mov:\.qt'
-  ${INSTALLDIR}/bin/cxnsplugin --bottle ${BOTTLE} --sync --mode 'ignore'
-  ${INSTALLDIR}/bin/wine --scope 'private' --no-convert --wl-app 'assocscan.exe' --scan --icon-dir '/home/crossover/.cxoffice/BaiduNetDisk/windata/Associations'
-  ${INSTALLDIR}/bin/wine '--scope' 'private' '--wl-app' 'pluginserver.exe' '--scandlls'
+# 安装ie8浏览器
+# prepare
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "iexplore.exe" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "itircl" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "itss" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "jscript" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "mlang" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "mshtml" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "msimtf" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "shdoclc" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "shdocvw" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "urlmon" /t REG_SZ /d "native, builtin" /f
+${WINE} Reg.exe add "HKCU\Software\Wine\DllOverrides" /v "inetcpl.cpl" /t REG_SZ /d "native, builtin" /f
+
+#if [ ! -f "$W_SYSTEM32_DLLS"/plugin.ocx ]; then
+if grep -q -i "wine placeholder" "$W_PROGRAMS_X86_UNIX/Internet Explorer/iexplore.exe"; then
+    ${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console iexplore -unregserver
+fi
+
+# Remove the fake DLLs from the existing WINEPREFIX
+if [ -f "$W_PROGRAMS_X86_UNIX/Internet Explorer/iexplore.exe" ]; then
+    mv "${HOME}/.cxoffice/${BOTTLE}/drive_c/Program Files/Internet Explorer/iexplore.exe" "${HOME}/.cxoffice/${BOTTLE}/drive_c/Program Files/Internet Explorer/iexplore.exe.bak"
+fi
+# for dll in browseui inseng itircl itss jscript msctf mshtml shdoclc shdocvw shlwapi urlmon
+# do
+#     test -f "$W_SYSTEM32_DLLS"/$dll.dll &&
+#     mv "$W_SYSTEM32_DLLS"/$dll.dll "$W_SYSTEM32_DLLS"/$dll.dll.bak
+# done
+
+# See https://bugs.winehq.org/show_bug.cgi?id=16013
+mkdir -p "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32/catroot/{f750e6c3-38ee-11d1-85e5-00c04fc295ee}"
+mv /tmp/exe/winetest.cat "${HOME}/.cxoffice/${BOTTLE}/drive_c/windows/system32/catroot/{f750e6c3-38ee-11d1-85e5-00c04fc295ee}/oem0.cat"
+
+${INSTALLDIR}/bin/wine IE8-WindowsXP-x86-ENU.exe "/quiet /forcerestart"
+
+# Work around DLL registration bug until ierunonce/RunOnce/wineboot is fixed
+# FIXME: whittle down this list
+for i in actxprxy.dll browseui.dll browsewm.dll cdfview.dll ddraw.dll \
+    dispex.dll dsound.dll iedkcs32.dll iepeers.dll iesetup.dll \
+    imgutil.dll inetcomm.dll isetup.dll jscript.dll laprxy.dll \
+    mlang.dll msctf.dll mshtml.dll mshtmled.dll msi.dll msimtf.dll msident.dll \
+    msoeacct.dll msrating.dll mstime.dll msxml3.dll occache.dll \
+    ole32.dll oleaut32.dll olepro32.dll pngfilt.dll quartz.dll \
+    rpcrt4.dll rsabase.dll rsaenh.dll scrobj.dll scrrun.dll \
+    shdocvw.dll shell32.dll urlmon.dll vbscript.dll webcheck.dll \
+    wshcon.dll wshext.dll asctrls.ocx hhctrl.ocx mscomct2.ocx \
+    plugin.ocx proctexe.ocx tdc.ocx uxtheme.dll webcheck.dll wshom.ocx
+do
+    ${INSTALLDIR}/bin/wine regsvr32 /i $i > /dev/null 2>&1
+done
+
+${WINE} Reg.exe add "HKCU\Software\Microsoft\Internet Explorer\Main" /v "TabProcGrowth" /t REG_DWORD /d "00000000" /f
+
+${INSTALLDIR}/bin/wine IE8-WindowsXP-KB2936068-x86-ENU.exe '/quiet /forcerestart'
+status=$?
+case $status in
+  0|194) ;;
+  *) echo "$W_PACKAGE installation failed"
+esac
+
+${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'msxml3=n' -- 'msiexec.exe' '/i' '/tmp/msxml3.msi' '/quiet'
+${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --untrusted --wait-children --no-convert --new-console --dll 'iexplore.exe=n' '--' '/tmp/IE8-WindowsXP-x86-ENU.exe'
+${INSTALLDIR}/bin/cxassoc --bottle ${BOTTLE} --sync
+# 开始菜单快捷方式
+${INSTALLDIR}/bin/wine --bottle ${BOTTLE} --wait-children --wl-app cxmklnk.exe -- --lnkfile 'C:\users\crossover\Start Menu//Internet Explorer.lnk' --target 'C:\Program Files/Internet Explorer/iexplore.exe'
+${INSTALLDIR}/bin/cxmenu --bottle ${BOTTLE} --sync --mode 'install'
+${INSTALLDIR}/bin/cxassoc --bottle ${BOTTLE} --sync --sync-mode --mode 'mime;default=\.asx:\.wma:\.wmx:\.asf:\.wm:\.wmv:\.wax;alternative=\.avi:\.mov:\.qt'
+${INSTALLDIR}/bin/cxnsplugin --bottle ${BOTTLE} --sync --mode 'ignore'
+# ${INSTALLDIR}/bin/wine --scope 'private' --no-convert --wl-app 'assocscan.exe' --scan --icon-dir '/home/crossover/.cxoffice/BaiduNetDisk/windata/Associations'
+${INSTALLDIR}/bin/wine --scope 'private' --wl-app 'pluginserver.exe' '--scandlls'
 
 #  wget https://raw.githubusercontent.com/john-shine/Docker-CodeWeavers_CrossOver-VNC/master/BaiduNetdisk/BaiduNetdisk.tar.xz -O /tmp/BaiduNetdisk.tar.xz && \
 tar -Jxf /tmp/installers/BaiduNetdisk.tar.xz -C ${HOME}/.cxoffice/${BOTTLE}/drive_c/Program\ Files/
--- BaiduNetdisk/run.sh
+++ BaiduNetdisk/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -d -p 5901:5901 johnshine/baidunetdisk-crossover-vnc:1.1
+sudo docker run -d --security-opt seccomp:unconfined -p 5901:5901 johnshine/baidunetdisk-crossover-vnc:1.1
